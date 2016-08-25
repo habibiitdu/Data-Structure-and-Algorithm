d54f30a2268736b8d959a4dab5b35f5c1ae3e167
habibiitdu <habib.iit.du@gmail.com>
2016-08-25 10:55:18 +0600

    Signleton pattern added

---
+++ Lesson03/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = new Singleton();
+
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            return instance;
+        }
+
+    }
+}
