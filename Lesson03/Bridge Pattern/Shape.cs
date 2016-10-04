03790a1c25742fddd31155e60812236face3e94b
habibiitdu <habib.iit.du@gmail.com>
2016-10-04 19:10:55 +0600

    1. Unwanted projects delted.

---
+++ Lesson03/Bridge Pattern/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Pattern
+{
+    public abstract class Shape
+    {
+        protected IDrawApi DrawApi; 
+        protected Shape(IDrawApi drawApi)
+        {
+            DrawApi = drawApi; 
+        }
+
+        public abstract void Draw(); 
+
+    }
+}
